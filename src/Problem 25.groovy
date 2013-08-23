c6a460936fcea1ebc32810726c18dc8c51a85c00
jamesmueller <jamesmueller@carfax.com>
2013-08-23 14:26:32 -0500

    "finished problem 25"

--- src/Problem 25.groovy
+++ src/Problem 25.groovy
@@ -6,3 +6,18 @@
  * Time: 1:05 PM
  * To change this template use File | Settings | File Templates.
  */
+
+
+def bigNumber = 10**999
+
+BigInteger current = 2, oneAgo = 1, iteration = 3
+println bigNumber.toString().length()
+
+while (current <= bigNumber) {
+    BigInteger newCurrent = oneAgo + current
+    oneAgo = current
+    current = newCurrent
+    iteration++
+    println "$iteration, $current, $oneAgo"
+}
+println iteration
